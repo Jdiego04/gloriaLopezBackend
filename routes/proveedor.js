68d75adafec3dc5dc777688d876d20f81408a96d
Jdiego04 <125164781+Jdiego04@users.noreply.github.com>
2023-06-14 23:02:17 -0500

    Crud de servicios

--- routes/proveedor.js
+++ routes/proveedor.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const consultas = require('../scripts/consultas')
 const pool = require('../views/database');
 
 
--- routes/servicio.js
+++ routes/servicio.js
@@ -1,85 +1,71 @@
 const express = require('express');
 const router = express.Router();
-
+const consultas = require('../scripts/consultas')
 const pool = require('../views/database');
 
-// Varios servicios
-router.get('/', (req, res) => {
-    let sql = 'SELECT * FROM SERVICIO'
-    pool.query(sql, (err, rows, fields) => {
-        if (err) throw err;
+//SERVICIOS
+router.get('/all', (req, res) => {
+    pool.query(consultas.SERVICIOS, (err, rows, fields) => {
+       if (err) throw err;
         else {
             res.json(rows)
         }
+        res.json('Hola');
     })
-})
-
-//Un servicio
-router.get('/:CODIGO_SERVICIO',(req,res)=>{
-    const{CODIGO_SERVICIO} = req.params;
-    let sql = 'SELECT * FROM SERVICIO WHERE CODIGO_SERVICIO= ?'
-    pool.query(sql,[CODIGO_SERVICIO], (err, rows, fields)=>{
-       if(err)throw err;
-       if(!rows[0]){
-        res.status(400).json({error: "Este dato no existe"})
-       }
-       else{
-        res.json(rows)
-       } 
-    })
-})
-
-//Insertar proveedor
-router.post('/', (req,res)=>{
-    const {CODIGO_SERVICIO, NOMBRE_SERVICIO	, COSTO_SERVICIO} = req.body
-    let sql = 'SELECT CODIGO_SERVICIO FROM SERVICIO WHERE CODIGO_SERVICIO= ?'
-    pool.query(sql,[CODIGO_SERVICIO], (err,rows,fields)=>{
-        if(err) return err;
-        if(rows[0]) return res.json({status: "error", error: "Este dato ya existe"})   
-    else{
-        let sql =`INSERT INTO SERVICIO(CODIGO_SERVICIO, NOMBRE_SERVICIO, COSTO_SERVICIO) 
-        values ('${CODIGO_SERVICIO}','${NOMBRE_SERVICIO}','${COSTO_SERVICIO}')`
-        pool.query(sql, (err, rows,fields)=>{
-            if(err) throw err
-            else {
-                res.json({status: 'servicio agregado'})
-            }
-        })  
-    }  
-    })     
 });
 
-
-//Delete
-router.delete('/:CODIGO_SERVICIO', (req,res)=>{
-    const {CODIGO_SERVICIO} = req.params
-
-    let sql = `DELETE FROM SERVICIO WHERE CODIGO_SERVICIO = '${CODIGO_SERVICIO}'`
-    pool.query(sql,(err,rows,fields)=>{
-        if(err) throw err
-        else{
-            res.json({status: 'Se elimino el servicio'})
+//servicio
+router.get('/servicio:id', (req, res) => {
+    const { id } = req.params
+    pool.query(consultas.SERVICIO,id, (err, rows, fields) => {
+       if (err) throw err;
+        else {
+            res.json(rows)
         }
+        res.json('Hola');
     })
 });
 
-//Modificar 
-router.put('/:ID', (req,res)=>{
-    const {ID} = req.params
-    const { NOMBRE_SERVICIO	, COSTO_SERVICIO} = req.body
 
-    let sql = `UPDATE SERVICIO SET
-    NOMBRE_SERVICIO = '${NOMBRE_SERVICIO}',
-    COSTO_SERVICIO = '${COSTO_SERVICIO}'
-    WHERE CODIGO_SERVICIO = '${ID}'`
-    
-    pool.query(sql,(err,rows,fields)=>{
+router.post('/servicio', (req,res) => {
+
+    const {nombre, valor} = req.body;
+  
+    pool.query(consultas.INSERTSERVICIO, 
+        [nombre, valor], (err, rows, fields) => {
+            if(!err){
+                res.json('Insertado correctamente');
+            }else{
+                console.log(err);
+            } 
+          })
+  });
+
+
+router.put('/desactivar', (req, res) => {
+    const {idProducto} = req.body
+
+    pool.query(consultas.DESSERVICIO, 
+        idProducto, (err, rows, fields) => {
         if (err) throw err
-        else{
-            res.json({status: 'Se modifico la informacion del servicio'})
+        else {
+            res.json({status: 'Se desactivo el producto'})
         }
     })
 })
 
+router.put('/update/:id', (req,res) => {
+    const id = req.params.id;
+    const {nombre, valor} = req.body;
+  
+      pool.query(consultas.UPDATESERVICIO, 
+        [nombre, valor, id], (err, rows, fields) => {
+          if(!err){
+              res.json('Actualizado correctamente');
+          }else{
+              console.log(err);
+          } 
+        })
+  });
 
 module.exports = router;
