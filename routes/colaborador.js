0a564d7db98fa4935ce6ed3b9deee5b98185299d
Jdiego04 <125164781+Jdiego04@users.noreply.github.com>
2023-10-27 19:13:15 -0500

    Conexion a bd de pruebas

--- routes/colaborador.js
+++ routes/colaborador.js
@@ -1,49 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const { connectToDatabase} = require('../views/database');
+const pool = require('../views/database');
 const keys = require('../views/keys');
 const consultas = require('../scripts/consultas')
 const util = require('../scripts/util/util')
 
 
 
-router.get('/all', async (req, res) => {
-    const connection = await connectToDatabase();
-    try{
-        const result = await connection.execute(consultas.EMPLEADOS);
-        res.json(result.rows);
-    }catch (err) {
-        console.error('Error al ejecutar la consulta:', err);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }finally {
-        if (connection) {
-            try {
-                await connection.close();
-            } catch (err) {
-                console.error('Error al cerrar la conexión:', err);
-            }
-        }
-    }
+router.get('/all', (req, res) => {
+  pool.query(consultas.EMPLEADOS, (err, rows, fields) => {
+      if (err) throw err;
+      else {
+          res.json(rows)
+      }
+  })
 });
 
-router.get('/empleado', async (req, res) => {
-    const { id } = req.body;
-    const connection = await connectToDatabase();
-    try{
-      const result = await connection.execute(consultas.EMPLEADO, [id]);
-      res.json(result.rows);
-    } catch (err) {
-        console.error('Error al ejecutar la consulta:', err);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error('Error al cerrar la conexión:', err);
-        }
+router.get('/empleado', (req, res) => {
+  const {id} = req.body;
+  pool.query(consultas.EMPLEADO, id,(err, rows, fields) => {
+      if (err) throw err;
+      else {
+          res.json(rows)
       }
-    }
-  });
+  })
+});
 
 module.exports = router;
--- views/keys.js
+++ views/keys.js
@@ -1,9 +1,11 @@
 //Manejamos las claves, palabras claves, etc del programa
 module.exports = {
     database: {
-        user: 'system',
-        password: '123',
-        connectString: 'localhost:1521/XEPDB1'
+        host: 'bcfvcghpnkkgf8cumvok-mysql.services.clever-cloud.com',
+        user: 'uwbkmke0zh8acojg',
+        password: '5Lf6fjJeNOhRaHe9MiT2',
+        port: 3306,
+        database: 'bcfvcghpnkkgf8cumvok'
     },
     secretEmp : 'gloria',
     secretUsu : 'lopez'
