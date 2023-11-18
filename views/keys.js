831f034efa371d276a9862e60c14c73d10da0edc
Jdiego04 <125164781+Jdiego04@users.noreply.github.com>
2023-11-18 17:09:11 -0500

    new db conecction

--- views/keys.js
+++ views/keys.js
@@ -1,11 +1,11 @@
 //Manejamos las claves, palabras claves, etc del programa
 module.exports = {
   database: {
-    host: "bcfvcghpnkkgf8cumvok-mysql.services.clever-cloud.com",
+    host: "bxbyioy2ogdebbw74gur-mysql.services.clever-cloud.com",
     user: "uwbkmke0zh8acojg",
     password: "5Lf6fjJeNOhRaHe9MiT2",
-    port: 3306,
-    database: "bcfvcghpnkkgf8cumvok",
+    port: 20919,
+    database: "bxbyioy2ogdebbw74gur",
   },
   secretEmp: "gloria",
   secretUsu: "lopez",
