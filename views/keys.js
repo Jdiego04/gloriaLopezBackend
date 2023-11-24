fa4182fcd8d47a74f55312287d6c712aef1ac1b4
brayan suarez <57419379+brayan2003@users.noreply.github.com>
2023-11-23 21:58:56 -0500

    keys

--- views/keys.js
+++ views/keys.js
@@ -3,6 +3,8 @@ module.exports = {
   database: {
     user: "us_gloriaLopezppi",
     password: "123456",
+ //   user: "US_GLORIALOPEZ",
+ //   password: "123456789",
     connectString: "localhost/XE",
   },
   secretEmp: "gloria",
